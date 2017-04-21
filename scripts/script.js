4f22046fbc28c2371c8e715efe9162a649114819
Kamilah Carter <kamilahcarter@kcarter.local>
2017-04-21 00:47:47 -0400

    Further development and jquery to deal with window heights

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,80 @@
-$(document).ready(function() {
+var AT = {
 
-	// Confirm javascript is enabled
-	$('body').removeClass('no-js');
-	$('body').addClass('js');
+	onReady: function() {
 
-	// Expand and hide sub menus when sections are opened
-    $('.main-nav > li').click(function() {
+		// Confirm javascript is enabled
+		$('body').removeClass('no-js');
+		$('body').addClass('js');
+
+		// Expand and hide sub menus when sections are opened
+		$('.main-nav > li').click(AT.activeMenu);
+
+		// Scroll smoothly when clicking on links
+    	$('a[href^="#"]').on('click', AT.smoothScroll);
+
+    	$(window).resize(AT.resizeMain);
+
+	},
+
+    activeMenu:	function() {
+    	// Only make changes if the section isn't already active
 		if (!$(this).hasClass("active")) {
+			// Remove active class from navigation
 			$('.main-nav > li').removeClass('active');
-	    	$('.sub-nav').slideUp("slow", function() {
-	    	});
-	    	$(this).next('.sub-nav').slideDown( "slow", function() {
-	    	});
+			// Animate showing and hiding sections
+	    	$('.sub-nav').slideUp("slow");
+	    	$(this).next('.sub-nav').slideDown( "slow");
+	    	// Add active calss to clicked link
 	    	$(this).addClass('active');
 	    }
-    });
+    },
 
-    $('a[href^="#"]').on('click',function (e) {
-    	e.preventDefault();
-    	
+    smoothScroll: function (event) {
+    	// Prevent default action
+    	event.preventDefault();
+    	// Store useful values
     	var target = this.hash;
    		$target = $(target);
-    	
     	var main = $('main');
-    	var initial = main.scrollTop()
-
+    	var initial = main.scrollTop();
+    	// Animate scrolling within the main element
     	main.stop().animate({
+    		//Set the scrollTop value to be the initial value plus the offset of the target
     		'scrollTop': initial + $target.offset().top
-    	}, 700, 'swing', function () {
+    	}, 700, 'swing', function (target) {
+    		// Set the window hash value to the target
     		window.location.hash = target;
     	});
-    });
+    },
 
+	resizeMain: function (lastWindowHeight) {
+	    // Get the windows height and width
+	    var thewindow = $(this)
+	    var windowHeight = thewindow.height();
+	    var windowWidth = thewindow.width();
+	    // Get the height of the #main and #navelements
+	    var main = parseInt($('main').css('height'));
+	    var nav = parseInt($('nav').css('height'));
+	    var smallBreakPoint = 400;
+	    // Set the height of main to be equal to the height of nav
+	    // If the window height is less then the nav height
+	    // And the window width is larger than the small break point
+	    if (windowHeight < nav && windowWidth > smallBreakPoint) {
+	   		if (main != nav) {
+			    $('main').css( 'height', nav + 'px' );
+			}
+	   	}
+	   	// If the previous conditions aren't met unset the height
+	    else {
+	    	$('main').css( 'height', '');
+    	}
+	}
 
-//    $('a[href^="#"]').on('click',function (e) {
-//        e.preventDefault();
-//
-//        var target = this.hash;
-//        $target = $(target);
-//
-//        $('main').stop().animate({
-//            'scrollTop': $target.offset().top
-//        }, 500, 'swing', function () {
-//            window.location.hash = target;
-//        });
-//    });
-});
+};
 
+$(document).ready(AT.onReady);
 
-$(window).load(function() {
-	// Animate loader off screen
-	$('#loader').addClass('hide');;
-});
+//$(window).load(function() {
+//	// Animate loader off screen
+//	$('#loader').addClass('hide');;
+//});
